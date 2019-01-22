cfad9e92c8d909460b9b6d9c4d5d875124e51724
aptoblop <darkrien.rolland@gmail.com>
2019-01-22 18:09:27 +0100

    premier test cheerio

--- castle.js
+++ castle.js
@@ -1,4 +1,7 @@
-var adresse = "https://www.relaischateaux.com/fr/destinations/europe/france";
-console.log(adresse);
+const cheerio = require('cheerio');
+
+//var adresse = "https://www.relaischateaux.com/fr/destinations/europe/france";
+const $ = cheerio.load('https://www.relaischateaux.com/fr/destinations/europe/france')
+console.log($);
 console.log("hello world");
 
