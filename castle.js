1346b370d2f0310cbbe6f1949f0ac1a88c3c704e
aptoblop <darkrien.rolland@gmail.com>
2019-01-22 20:56:50 +0100

    bien avancé, mais bon ...

--- castle.js
+++ castle.js
@@ -1,7 +1,26 @@
+var request=require("request");
 const cheerio = require('cheerio');
+const parse5=require('parse5');
+const jsdom=require('jsdom');
+const rp=require('request-promise');
+var listFrance;
 
 //var adresse = "https://www.relaischateaux.com/fr/destinations/europe/france";
-const $ = cheerio.load('https://www.relaischateaux.com/fr/destinations/europe/france')
-console.log($);
-console.log("hello world");
+rp('https://www.relaischateaux.com/fr/destinations/europe/france').then((html) => {
+
+    var fourChildren=cheerio('#countryF',html).children()
+
+    fourChildren.find('h3').each(function(i,elem){
+        if(cheerio(this).text()=="France"){
+            listFrance=cheerio(this).next();
+        }
+    })
+
+    
+
+})
+//const $ = cheerio.load('https://www.relaischateaux.com/fr/destinations/europe/france');
+//console.log($)
+//console.log("hello world");
+console.log(listFrance)
 
